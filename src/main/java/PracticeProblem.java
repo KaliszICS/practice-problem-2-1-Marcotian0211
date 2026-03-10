705aaade26418adb6ce6610a2f816ffb5c0e98f9
Marcotian0211 <350327743@tdsb.ca>
2026-03-10 15:11:35 +0000

    update practice problem2-1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,24 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	
 
 	}
 
 	//Create your functions here
+	public static int sum(int num1,int num2) {
+    return num1+num2;
+    }
+	public static int difference(int num1, int num2) {
+		return num1-num2;
+	}
+	public static double product(double num1, double num2){
+     return num1*num2;
+	}
+	public static String removeFirst (String word){
+		String result;
+		result=word.substring(1);
+		return result;
 
+	}
 }
